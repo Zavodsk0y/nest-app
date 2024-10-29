a2f76b5bcfebe76828f66ec030f43acb4342dfbe
Zavodsk0y <zimniyvolk5@gmail.com>
2024-10-30 00:07:20 +0700

    product resource init

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,14 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductController } from './product/product.controller';
+import { ProductService } from './product/product.service';
 
 describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-      providers: [AppService],
+      controllers: [AppController, ProductController],
+      providers: [AppService, ProductService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
