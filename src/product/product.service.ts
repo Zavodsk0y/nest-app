2b10904621c2ab7a132e4eedbbc18d0a62b864de
Zavodsk0y <zimniyvolk5@gmail.com>
2024-10-30 00:19:43 +0700

    get-method for all products, lint fixes

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+    getProducts(): Array<object> {
+        return [{"id": 1, "name": "Dishes"}, {"id": 2, "name": "Sofa"}, {"id": 3, "name": "TV"}]
+    }
+}
