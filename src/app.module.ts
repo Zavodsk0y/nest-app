477a4a96e753aeabced597b87c8fbc36107b8d82
Zavodsk0y <zimniyvolk5@gmail.com>
2024-10-30 14:25:02 +0700

    Validation start - class-validator package - added validation in CreateProductDto - redefining useGlobalPipes for another ValidationPipe httpstatus (422)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 import { ProductController } from './product/product.controller';
+import { APP_PIPE } from '@nestjs/core';
 
 @Module({
   imports: [ProductModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,7 +1,11 @@
 import { PartialType } from "@nestjs/mapped-types";
+import { IsNumber, IsString } from "class-validator";
 
 export class CreateProductDto {
+    @IsNumber()
     id: number;
+
+    @IsString()
     name: string;
 }
 
