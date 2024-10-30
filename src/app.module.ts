678db48ddb616ebfafd807514efb5f3edf06f0fe
Zavodsk0y <zimniyvolk5@gmail.com>
2024-10-30 14:13:30 +0700

    - work with exception, trying to create custom Forbidden Exception - additional updateDto within PartialType - logger middleware for /product routes (full controller)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { LoggerMiddleware } from './middleware/logger.middleware';
+import { ProductController } from './product/product.controller';
 
 @Module({
   imports: [ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .forRoutes(ProductController)
+  }
+}
--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,4 +1,8 @@
+import { PartialType } from "@nestjs/mapped-types";
+
 export class CreateProductDto {
     id: number;
     name: string;
 }
+
+export class UpdateProductDto extends PartialType(CreateProductDto) {}
